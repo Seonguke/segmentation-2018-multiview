4cb9f5e0d0e7e104f77a37a468ece722ef8c961d
carlesventura <carles.ventura@upc.edu>
2016-05-20 12:58:50 +0200

    Add files via upload

---
+++ co-clustering-sequence/aux/mex/RAG_Inter_motion_estimation_optical_flow_mex.cpp
@@ -0,0 +1,118 @@
+/*
+------------------------------------------------------------------------
+  Copyright (C)
+  Universitat Politecnica de Catalunya BarcelonaTech (UPC) - Spain
+
+  Andreu Girbau <andreu.girbau@alu-etsetb.upc.edu>
+  September 2014
+------------------------------------------------------------------------
+ */
+
+
+#include "mex.h"
+#include "matlab_multiarray.hpp"
+#include <iostream>
+#include <list>
+#include <set>
+#include <algorithm>
+#include "matrix.h"
+#include "string.h"
+
+using namespace std;    // For std::vector
+
+
+
+void mexFunction( int nlhs, mxArray *plhs[],
+        int nrhs, const mxArray*prhs[] )
+{
+    if(nrhs != 4)
+        mexErrMsgTxt("There should be 4 inputs (partition1, partition2, MV_x and MV_y)");
+       
+    
+    double *partition1, *partition2;
+    int N_reg1, N_reg2;
+    
+    
+    /* Input parameters
+     * prhs[0] -> partition      (double(matrix))
+     * prhs[1] -> partition2     (double(matrix))
+     * prhs[2] -> motion_estimation_vectors     (double(matrix))
+     */
+     
+    partition1 = mxGetPr(prhs[0]);
+    partition2 = mxGetPr(prhs[1]);
+    int numelems = mxGetNumberOfElements(prhs[2]); 
+    
+    int m = mxGetM(prhs[0]);
+    int n = mxGetN(prhs[0]);
+    
+    cout << m << " " << n << endl;
+    
+    
+//     mxArray *partition1_data_pr = mxCreateDoubleMatrix(m,n,mxREAL);
+//     MatlabMultiArray<double> partition1_data(partition1_data_pr);
+//     
+//     mxArray *partition2_data_pr = mxCreateDoubleMatrix(m,n,mxREAL);
+//     MatlabMultiArray<double> partition2_data(partition2_data_pr);
+
+    
+    N_reg1 = 0;
+    N_reg2 = 0;
+    
+    
+    for(int i = 0; i < m*n; i++){
+        
+        if(partition1[i] > N_reg1)
+            N_reg1 = partition1[i];
+
+        if(partition2[i] > N_reg2)
+            N_reg2 = partition2[i];  
+        
+    }
+
+    
+    /* Output allocation
+     * plhs[0] -> RAG_Intra         (double(matrix))
+     * plhs[1] -> RAG_Inter         (double(matrix))
+     */
+    
+    
+    plhs[0] = mxCreateDoubleMatrix(N_reg1,N_reg2,mxREAL);
+    MatlabMultiArray<double> RAG_Inter(plhs[0]);
+    
+    
+    double *MV_x_mat;
+    MV_x_mat = mxGetPr(prhs[2]);
+    
+    double *MV_y_mat;
+    MV_y_mat = mxGetPr(prhs[3]);
+    
+    // INTER
+    
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            int region_label_2 = partition2[i + j*m];
+            double MV_x = MV_x_mat[i + j*m];
+            double MV_y = MV_y_mat[i + j*m];
+
+            int new_col_pos = j + round(MV_x);
+            int new_row_pos = i + round(MV_y);
+            if (new_col_pos < 0)
+                new_col_pos = 0;
+            if (new_row_pos < 0)
+                new_row_pos = 0;
+            if (new_col_pos >= n)
+                new_col_pos = n-1;
+            if (new_row_pos >= m)
+                new_row_pos = m-1;
+            
+            RAG_Inter[partition1[new_row_pos + new_col_pos*m]-1][partition2[i + j*m]-1] = 1;
+        }
+    }
+    
+//     mxDestroyArray(partition1_data_pr);
+//     mxDestroyArray(partition2_data_pr);
+//     mxDestroyArray(motion_estimation_v);
+    
+    
+}
