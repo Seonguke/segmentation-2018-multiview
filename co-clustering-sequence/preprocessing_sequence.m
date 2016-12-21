e170f98002452529f22b3434bacc56062de2c527
cventura <carles.ventura@upc.edu>
2016-12-21 10:03:57 +0100

    checking whether ucm have been already computed

--- co-clustering-sequence/preprocessing_sequence.m
+++ co-clustering-sequence/preprocessing_sequence.m
@@ -19,9 +19,11 @@ for jj=1:numel(frames)
     image_filename = fullfile(video_dir,frames(jj).name);
     [path_img_file, img_basename, img_extension] = fileparts(image_filename);
     ucm_filename = fullfile(ucm_video_path,[img_basename '.mat']);
-    [ucm, ws_a] = im2ucm_normal_vectors(image_filename,ucm_filename);
-    vector_filename = fullfile(ucm_vectors_video_path,[img_basename '.mat']);
-    save(vector_filename,'ws_a');
+    if ~exist(ucm_filename) 
+        [ucm, ws_a] = im2ucm_normal_vectors(image_filename,ucm_filename);
+        vector_filename = fullfile(ucm_vectors_video_path,[img_basename '.mat']);
+        save(vector_filename,'ws_a');
+    end
 end
 
 %% Computing optical flow between consecutive frames
